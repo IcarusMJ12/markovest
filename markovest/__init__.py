63d72d25e8d90e3e46aec785844f7244bb4876a6
Igor Kaplounenko <igor@nervanasys.com>
2017-08-26 22:04:56 -0400

    tf-idf-y stuff almost there

--- markovest/__init__.py
+++ markovest/__init__.py
@@ -39,18 +39,21 @@ class MaximumRetriesReachedError(Exception):
 
 
 class Word(str):
-    __slots__ = ['text_ids']
+    __slots__ = ['text_id', 'intensity']
 
     def __new__(cls, string, *argv):
         return super(Word, cls).__new__(cls, string)
 
-    def __init__(self, string, text_ids):
-        self.text_ids = text_ids
+    def __init__(self, string, text_id, intensity=1):
+        self.text_id = text_id
+        self.intensity = intensity
 
     def colorize(self):
         rgb = [0, 0, 0]
-        for text_id in self.text_ids:
-            rgb[text_id] = 0xff
+        if self.intensity > 0:
+            rgb[self.text_id] = 0xff
+        else:
+            rgb = [0xff, 0xff, 0xff]
         return rgb8(*rgb) + self
 
 
@@ -66,6 +69,16 @@ class Chain(object):
         self._starts = set()
         self._ends = set()
         self._seen_sentences = set()
+        # not-really tf-idf, because it's equally-weighted
+        # frequency per document / overall frequency
+        self._tfs = []  # per document
+        # inverse *total* frequency
+        # this is normalized by including a phony document that has one of each
+        # possible word
+        self._itf = defaultdict(lambda: 0)
+        # will contain a dict of (doc_id, tf/itf) tuples
+        # tf/itf ranges from 0 to <1
+        self._weighings = None
 
     def save(self, fname):
         with open(fname, 'wb') as f:
@@ -79,8 +92,24 @@ class Chain(object):
     def _update_key(self, key, word):
         return key[-(self._link_size-1):] + (word,)
 
+    def _update_word_frequency(self, text):
+        tf = defaultdict(lambda: 0)
+        total = 0
+        for sent in text:
+            for word in sent:
+                if len(word) < 3:
+                    continue
+                word = word.lower()
+                total += 1
+                tf[word] += 1
+        for key in tf.keys():
+            tf[key] /= total
+            self._itf[key] += tf[key]
+        self._tfs.append(tf)
+
     def add_text(self, text, text_id=0):
         text = [tuple(word_tokenize(sent)) for sent in sent_tokenize(text)]
+        self._update_word_frequency(text)
         for sent in text:
             self._seen_sentences.add(
                     detokenizer.detokenize(sent, return_str=True))
@@ -103,10 +132,27 @@ class Chain(object):
     def make_sentence(self, retries=100):
         return self.make_sentences(retries=retries)
 
+    def _maybe_build_weighings(self):
+        if self._weighings is not None:
+            return
+        self._weighings = defaultdict(lambda: (0, 0))
+        maximum = 0
+        adjustment = 1.0/len(self._itf)
+        for key in self._itf.keys():
+            self._itf[key] += adjustment
+            maximum = max((maximum, self._itf[key]))
+        for key in self._itf.keys():
+            candidates = [self._tfs[i][key] for i in range(len(self._tfs))]
+            highest = max(candidates)
+            self._weighings[key] = (candidates.index(highest), highest/maximum)
+
     def make_sentences(self, count=1, retries=100):
+        self._maybe_build_weighings()
         for _ignored in range(retries):
+            covered_texts = set()
             key = random.choice(tuple(self._starts))
-            words = [Word(w, self._tuple_to_text_id[key]) for w in key]
+            covered_texts.update(self._tuple_to_text_id[key])
+            words = [Word(w, *self._weighings[w]) for w in key]
             sent_count = 0
             prev_sentence_end = 0
             while True:
@@ -114,7 +160,8 @@ class Chain(object):
                     key = random.choice(tuple(self._tuple_to_nxt[key]))
                 except IndexError:
                     break
-                words += [Word(key[-1], self._tuple_to_text_id[key])]
+                covered_texts.update(self._tuple_to_text_id[key])
+                words += [Word(key[-1], *self._weighings[key[-1]])]
                 if key in self._ends:
                     if detokenizer.detokenize(
                             words[prev_sentence_end:],
